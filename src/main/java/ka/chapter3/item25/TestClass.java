b2798b482cfb98cba9f94688ed2eb14f8c7a299e
Jwhyee <kawnsdud@naver.com>
2023-10-24 19:46:46 +0900

    add : [item25] - 톱레벨 클래스는 한 파일에 하나만 담으라.

---
+++ src/main/java/ka/chapter3/item25/TestClass.java
@@ -0,0 +1,8 @@
+package ka.chapter3.item25;
+
+public class TestClass {
+    public static void main(String[] args) {
+        final String result = Utensil.NAME + Dessert.NAME;
+        System.out.println(result);
+    }
+}
