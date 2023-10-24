95d9ba2ce257d442efdd955c5874692e2f955a78
Jwhyee <kawnsdud@naver.com>
2023-10-24 19:20:13 +0900

    add : [item24] - 멤버 클래스는 되도록 static으로 만들라.

---
+++ src/main/java/ka/chapter3/item24/staticclass/ColorTest.java
@@ -0,0 +1,12 @@
+package ka.chapter3.item24.staticclass;
+
+import org.junit.jupiter.api.Test;
+
+public class ColorTest {
+    @Test
+    void test() {
+        Color c = new Color();
+        c.addPos(10, 20);
+
+    }
+}
